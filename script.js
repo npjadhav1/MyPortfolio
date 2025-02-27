5a11c18b561b6822ae3f85a92103bfc87d915fe8
Nikita Jadhav <npjadhav1011@gmail.com>
2025-02-27 21:48:38 +0530

    home page design completed

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Android Developer","Frontend Developer","FullStack Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
